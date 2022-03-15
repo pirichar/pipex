ba42698805263f73fed9f917c29b2aee9f88a61b
WSL PL <plrichard@me.com>
2022-03-15 16:26:05 -0400

    end of the day 15 mars

--- redirecting_std_output_in_c.c
+++ redirecting_std_output_in_c.c
@@ -9,6 +9,24 @@
 /*
 	https://www.youtube.com/watch?v=5fnVr-zH-SE&list=PLfqABt5AS4FkW5mOn2Tn9ZZLLDwA3kZUY&index=14	https://www.youtube.com/watch?v=DiNmwwQWl0g&list=PLfqABt5AS4FkW5mOn2Tn9ZZLLDwA3kZUY&index=12
 	int this I add an int file which I send open to
+	in order to redirect the output of a process
+
+	SO YOU DONT WANT TO USE THIS BUT IT EXISTS EH !
+	You can use dup(file) to duplicate the fd
+	It will return another file descriptor
+	which will point to the same thing as the first one
+	you could use on or another for read or write 
+
+	OR
+
+	You can use dup2(file, 1)
+	This function takes 2 parametes
+	The first is fd we actually want to clone
+	The second one is the value we one the new FD to have
+	so if we use dup2(file, 1) instead of creating a new value
+	its gonna take a look at fd1 and close this stream (stdout)
+	then its gonna open it again to our ping result
+	This way we finally duplicate the new created FD into the stdout
 */
 
 int main(int argc, char **argv, char **envp)
@@ -18,7 +36,8 @@ int main(int argc, char **argv, char **envp)
 	int wstatus;
 	int status_code;
 	int file;
-	char *vec[] = {"/sbin/ping", "-c", "3", "google.com", NULL};
+	int file2;
+	char *vec[] = {"/bin/ping", "-c", "3", "google.com", NULL};
 	
 
 	pid = fork();
@@ -29,14 +48,25 @@ int main(int argc, char **argv, char **envp)
 	if(pid == 0) 
 	{
 		//here I use open to give the fd of the file to the in
+		// Doing so I create a new FD which I will use later on
 		file = open("pingResult.txt", O_WRONLY | O_CREAT, 0777);
 		if (file == -1)
 			return (2);
-
+		//here I use dup2 to duplicate the newly created FD (lets say 3)
+		//into the stdout this way when you use write or printf or anything
+		//that would use the stdout it will go to the file instead
+		//this work because before even the first line of main
+		//stdout is set to 1 in linux (stdin = 0 and error are 2)
+		//Finally we could change the one for STDOUT_FILENO which
+		//is a constant in linux and will give 1 as well
+		printf("The fd to ping_result.txt : %d\n", file);
+		file2 = dup2(file, STDOUT_FILENO);
+		printf("The duplicated fd: %d\n", file2);
+		close(file);
 		//here we use the fact that execve
 		//return -1 if it does not find the command
 		//lets say i typed pingr instead of ping
-		err = execve("/sbin/ping", vec, envp);
+		err = execve("/bin/ping", vec, envp);
 		if (err == -1)
 		{
 			printf("Could not find program to execute\n");
